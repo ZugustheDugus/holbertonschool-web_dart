38300e2b831523a633a5c4951c8e68b14e530083
ZugustheDugus <h1cooper2019@protonmail.com>
2023-02-20 19:37:01 -0600

    task 6

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  var number = int.parse(args[0]);
+  /*
+  Write Your code below 
+    */
+    if number > 0 {
+      print("${number} is positive");
+    }
+    else if number < 0 {
+      print("${number} is negative");
+    }
+    else {
+      print("${number} is zero")
+    }
+}
